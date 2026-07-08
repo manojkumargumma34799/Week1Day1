99bcccebfbae6ac112f94eab073785a7445ae90b
manojkumargumma34799 <manojkumargumma34799@gmail.com>
2026-07-08 14:17:52 +0530

    Week1Day1 Assignment
    Java script basics

---
+++ javascriptbasics.js
@@ -0,0 +1,20 @@
+let firstName = "Gumma Manoj Kumar"
+console.log(firstName)
+console.log(typeof firstName)
+
+let companyName = "Dynpro"
+console.log(companyName)
+console.log(typeof companyName)
+
+let mobileNumber = 9491233055
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+let isAutomation = true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
+let hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
+
